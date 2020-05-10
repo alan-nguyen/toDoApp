4d15faeb5a5372c57b203307ad1b55c306a58995
Alan Nguyen <tuan8101@gmail.com>
2020-05-09 23:24:07 -0600

    Render data from db

--- app.py
+++ app.py
@@ -17,10 +17,4 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
