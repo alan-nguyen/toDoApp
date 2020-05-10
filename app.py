fbd64ac722fd89dac1f76dbc3b7ef5e8301f5aa7
Alan Nguyen <tuan8101@gmail.com>
2020-05-09 23:04:30 -0600

    Create class Todo

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
 
 
 @app.route('/')
