9d37163b9858cad5b1983c18c3b2aca1ef1af3c4
Alan Nguyen <tuan8101@gmail.com>
2020-05-10 00:58:48 -0600

    Fix error

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ db.create_all()
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
+    body = {}
     try:
         description = request.get_json()['description']
         todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
-
+        body['description'] = todo.description
     except:
         error = True
         db.session.rollback()
@@ -37,9 +38,7 @@ def create_todo():
     finally:
         db.session.close()
     if not error:
-        return jsonify({
-            'description': todo.description
-        })
+        return jsonify(body)
 
 # Route for index
 @app.route('/')
