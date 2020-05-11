66aaa7b58c20f5ac74db3e7a557f8d4b3a893953
Alan Nguyen <tuan8101@gmail.com>
2020-05-11 09:26:32 -0600

    Complete setting up route for set-completed

--- app.py
+++ app.py
@@ -61,3 +61,10 @@ def index():
 def set_completed_todo(todo_id):
     try:
         completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
