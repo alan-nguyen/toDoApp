dd31926ae43c23dbb5ad61de8d929f84f9b92e9b
Alan Nguyen <tuan8101@gmail.com>
2020-05-12 23:33:13 -0600

    Create TodoList class

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
 
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+
 # Create table
 db.create_all()
 
